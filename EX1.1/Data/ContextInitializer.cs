2d0bfba72bc38f7c394d5e4b1a9696afa60292b0
Alexandra Damaschin <damaschin.alexandra.maria@gmail.com>
2018-01-22 15:18:46 +0000

    database seed data

--- EX1.1/Data/ContextInitializer.cs
+++ EX1.1/Data/ContextInitializer.cs
@@ -1,11 +1,25 @@
-﻿using System;
+﻿using EX1._1.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace EX1._1.Data
 {
-    public class ContextInitializer
+    public class ContextInitializer : DropCreateDatabaseIfModelChanges<CoreContext>
     {
+        protected override void Seed(CoreContext context)
+        {
+            var users = new List<User> {
+                new User() {FirstName= "John", MidName="Frank", LastName="Doe", DOB = DateTime.Parse("1980-01-01") },
+                new User() {FirstName= "Jane",MidName="Frank", LastName="Doe", DOB = DateTime.Parse("1986-01-01") },
+                new User() {FirstName= "Mark",MidName="Frank", LastName="Smith", DOB = DateTime.Parse("1972-01-01") },
+                new User() {FirstName= "Mary",MidName="Frank", LastName="Smith", DOB = DateTime.Parse("1975-01-01") }
+            };
+
+            users.ForEach(u => context.Users.Add(u));
+            base.Seed(context);
+        }
     }
 }
