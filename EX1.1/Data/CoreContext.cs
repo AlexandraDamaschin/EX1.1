c5a386c7fd37b4df989be483c4db39913562e328
Alexandra Damaschin <damaschin.alexandra.maria@gmail.com>
2018-01-22 15:15:47 +0000

    context

---
+++ EX1.1/Data/CoreContext.cs
@@ -0,0 +1,24 @@
+﻿using EX1._1.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace EX1._1.Data
+{
+    public class CoreContext : DbContext
+    {
+        public DbSet <User> Users { get; set; }
+
+        public CoreContext (): base ("CoreContext")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
