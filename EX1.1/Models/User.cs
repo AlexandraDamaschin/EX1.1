a4e36c5622986c58a1ba5fd690b6720ed1adca16
Alexandra Damaschin <damaschin.alexandra.maria@gmail.com>
2018-01-22 15:11:03 +0000

    User tabel created

---
+++ EX1.1/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EX1._1.Models
+{
+    [Table("Book")]
+    public class User
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string MidName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
